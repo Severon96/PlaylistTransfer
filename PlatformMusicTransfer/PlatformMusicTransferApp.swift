90ec031eaa20c0d472c90174b4c1874b6a5da466
Dominik Mack <dominik.mack@icloud.com>
2020-11-04 16:35:16 +0100

    Initial Commit

---
+++ PlatformMusicTransfer/PlatformMusicTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatformMusicTransferApp.swift
+//  PlatformMusicTransfer
+//
+//  Created by Dominik Mack on 04.11.20.
+//
+
+import SwiftUI
+
+@main
+struct PlatformMusicTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
